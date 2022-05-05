c6904d72c9e107101db579096789b9ce428ffec8
Ahmad Fares <98591677+AhmadFares@users.noreply.github.com>
2022-05-05 10:41:23 +0300

    Design and creating local db

--- app/src/main/java/com/example/exams/MainActivity.java
+++ app/src/main/java/com/example/exams/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.exams;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+
+            SQLiteDatabase sql = this.openOrCreateDatabase("Exams", MODE_PRIVATE, null);
+            sql.execSQL("CREATE Table IF NOT EXISTS exams (name VARCHAR)");
+        sql.execSQL("INSERT INTO exams(name) VALUES ('MobileComputing')");
+        sql.execSQL("INSERT INTO exams(name) VALUES ('Game')");
+
+
+
+    }catch(Exception e){
+        e.printStackTrace();
+    }
     }
 }
