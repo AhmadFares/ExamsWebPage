761d051f57cc0ee601b464da69f84348c423e64f
Ahmad Fares <98591677+AhmadFares@users.noreply.github.com>
2022-05-05 12:25:22 +0300

    Finalizing stuff

--- app/src/main/java/com/example/exams/MainActivity.java
+++ app/src/main/java/com/example/exams/MainActivity.java
@@ -36,31 +36,31 @@ public class MainActivity extends AppCompatActivity {
                 String selectedItem = (String) adapterView.getItemAtPosition(i);
                 Log.i(selectedItem, "akal");
                 if(selectedItem.equals("        Game ")){
-                    String URRL="http://www.lau.edu.lb";
+                    String URRL="https://www.edx.org/learn/game-development";
                     Intent obj = new Intent(getApplicationContext(), Web.class);
                     obj.putExtra("GameUrl", URRL);
                     startActivity(obj);
                 }
                 if(selectedItem.equals("        MobileComputing ")){
-                    String URRL="http://www.lau.edu.lb";
+                    String URRL="https://ionicframework.com/";
                     Intent obj = new Intent(getApplicationContext(), Web.class);
                     obj.putExtra("GameUrl", URRL);
                     startActivity(obj);
                 }
                 if(selectedItem.equals("        NLP ")){
-                    String URRL="http://www.lau.edu.lb";
+                    String URRL="https://www.codecademy.com/learn/natural-language-processing?g_network=g&g_device=c&g_adid=525783088229&g_keyword=natural%20language%20learning&g_acctid=243-039-7011&g_adtype=search&g_campaign=ROW+Language%3A+Pro+-+Exact&g_keywordid=kwd-296866638144&g_campaignid=10074200768&g_adgroupid=102650145273&utm_id=t_kwd-296866638144:ag_102650145273:cp_10074200768:n_g:d_c&utm_term=natural%20language%20learning&utm_campaign=ROW%20Language%3A%20Pro%20-%20Exact&utm_source=google&utm_medium=paid-search&utm_content=525783088229&hsa_acc=2430397011&hsa_cam=10074200768&hsa_grp=102650145273&hsa_ad=525783088229&hsa_src=g&hsa_tgt=kwd-296866638144&hsa_kw=natural%20language%20learning&hsa_mt=e&hsa_net=adwords&hsa_ver=3&gclid=CjwKCAjw682TBhATEiwA9crl38LyyS5MAVZqCNPyvEm0EohyLDx9f6e95HeK-bSRGXDY2XMw4DVVvxoCJbYQAvD_BwE";
                     Intent obj = new Intent(getApplicationContext(), Web.class);
                     obj.putExtra("GameUrl", URRL);
                     startActivity(obj);
                 }
                 if(selectedItem.equals("        Actuarial Mathematics ")){
-                    String URRL="http://www.lau.edu.lb";
+                    String URRL="https://www.edx.org/learn/actuarial-science";
                     Intent obj = new Intent(getApplicationContext(), Web.class);
                     obj.putExtra("GameUrl", URRL);
                     startActivity(obj);
                 }
                 if(selectedItem.equals("        Discrete II ")){
-                    String URRL="http://www.lau.edu.lb";
+                    String URRL="https://www.codecademy.com/learn/discrete-math";
                     Intent obj = new Intent(getApplicationContext(), Web.class);
                     obj.putExtra("GameUrl", URRL);
                     startActivity(obj);
